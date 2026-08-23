6008048c4470f076f3992cb696e5b823f7f7698f
dr-cmyk-sozo <oluadeyemo@sozorockfoundation.org>
2026-08-22 23:33:09 -0400

    test: expose workspace decision application service

--- python/cbcap_core/tests/test_package_api.py
+++ python/cbcap_core/tests/test_package_api.py
@@ -2,7 +2,7 @@ import cbcap_core
 from cbcap_core import decision_memory, institutional_memory
 
 
-def test_package_exports_governed_memory_trajectory_forecast_and_runtime_surface():
+def test_package_exports_governed_memory_trajectory_forecast_workspace_and_runtime_surface():
     required = {
         "DecisionMemoryRecord",
         "DecisionMemoryProposal",
@@ -40,6 +40,10 @@ def test_package_exports_governed_memory_trajectory_forecast_and_runtime_surface
         "persist_forecast_backtest_policy",
         "persist_backtest_policy_evaluation",
         "persist_forecast_model_approval",
+        "WorkspaceDecisionRequest",
+        "WorkspaceDecisionResult",
+        "prepare_workspace_decision",
+        "record_workspace_decision",
         "EvidenceGatewayHttpClient",
         "EvidenceGatewayTransportError",
         "EvidenceGatewayFetchResult",
