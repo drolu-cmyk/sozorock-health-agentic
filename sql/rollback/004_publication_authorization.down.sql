b490f16694b97caf203ccc10d89156bd3047df8d
dr-cmyk-sozo <oluadeyemo@sozorockfoundation.org>
2026-08-22 23:45:34 -0400

    fix: refuse destructive publication authorization rollback

--- sql/rollback/004_publication_authorization.down.sql
+++ sql/rollback/004_publication_authorization.down.sql
@@ -1,9 +1,20 @@
 BEGIN;
 
+DO $$
+BEGIN
+  IF EXISTS (SELECT 1 FROM cbcap.publication_authorization LIMIT 1) THEN
+    RAISE EXCEPTION
+      'Refusing to drop CB-CAP publication authorization history because approval records exist. Export or migrate them explicitly before rollback.';
+  END IF;
+END $$;
+
+DROP TRIGGER IF EXISTS publication_authorization_memory_guard
+  ON cbcap.publication_authorization;
 DROP TRIGGER IF EXISTS publication_authorization_append_only
   ON cbcap.publication_authorization;
 DROP POLICY IF EXISTS publication_authorization_tenant_isolation
   ON cbcap.publication_authorization;
 DROP TABLE IF EXISTS cbcap.publication_authorization;
+DROP FUNCTION IF EXISTS cbcap.validate_publication_authorization_memory();
 
 COMMIT;
